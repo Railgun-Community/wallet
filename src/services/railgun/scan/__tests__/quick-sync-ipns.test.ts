dd9f08e99786043337c6a782080aea0045d297f5
jrmeurer <jrmeurer@wisc.edu>
2022-11-02 08:31:14 -0600

    Add test for data gaps

--- src/services/railgun/scan/__tests__/quick-sync-ipns.test.ts
+++ src/services/railgun/scan/__tests__/quick-sync-ipns.test.ts
@@ -1,4 +1,4 @@
-import { Chain, ChainType } from '@railgun-community/engine';
+import { Chain, ChainType, CommitmentEvent } from '@railgun-community/engine';
 import axios from 'axios';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
@@ -14,7 +14,45 @@ const POLYGON_CHAIN: Chain = { type: ChainType.EVM, id: 137 };
 const EXPECTED_COMMITMENT_EVENTS = 2500;
 const EXPECTED_NULLIFIER_EVENTS = 2600;
 
-describe.only('quick-sync-ipns', () => {
+describe('quick-sync-ipns', () => {
+  it('Should make sure IPNS Event Log has no data gaps in commitments', async () => {
+    const eventLog = await quickSyncIPNS(
+      POLYGON_CHAIN,
+      0,
+      QuickSyncPageSize.Small,
+    );
+    expect(eventLog).to.be.an('object');
+    expect(eventLog.commitmentEvents).to.be.an('array');
+
+    eventLog.commitmentEvents.sort((a: CommitmentEvent, b: CommitmentEvent) => {
+      return a.treeNumber < b.treeNumber || a.startPosition < b.startPosition
+        ? -1
+        : 1;
+    });
+    let nextTreeNumber = eventLog.commitmentEvents[0].treeNumber;
+    let nextStartPosition = eventLog.commitmentEvents[0].startPosition;
+    eventLog.commitmentEvents.forEach(event => {
+      if (
+        event.treeNumber !== nextTreeNumber &&
+        event.startPosition !== nextStartPosition
+      ) {
+        throw new Error(
+          `Could not find treeNumber ${nextTreeNumber}, startPosition ${nextStartPosition}`,
+        );
+      }
+      nextStartPosition += event.commitments.length;
+      if (nextStartPosition >= 65536) {
+        // Roll over to next tree.
+        nextTreeNumber += 1;
+        nextStartPosition = 0;
+      }
+    });
+
+    // console.log(
+    //   `checked ${eventLog.commitmentEvents.length} commitments for data gaps`,
+    // );
+  }).timeout(90000);
+
   it('Should run live Railgun Event Log fetch for Polygon from block 0 - Small', async () => {
     const eventLog = await quickSyncIPNS(
       POLYGON_CHAIN,
