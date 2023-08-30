7e817733f7d40b1b15c23c708c5d3204dde5b47f
jrmeurer <jrmeurer@wisc.edu>
2023-08-30 09:06:45 -0600

    Add getAllShields

---
+++ src/services/railgun/core/shields.ts
@@ -0,0 +1,29 @@
+import { NETWORK_CONFIG, NetworkName } from '@railgun-community/shared-models';
+import { getEngine } from './engine';
+
+export type ShieldData = {
+  txid: string;
+  hash: string;
+  timestamp: Optional<number>;
+};
+
+export const getAllShields = async (
+  networkName: NetworkName,
+  startingBlock: number,
+) => {
+  const engine = getEngine();
+  const { chain } = NETWORK_CONFIG[networkName];
+  const shieldCommitments = await engine.getAllShieldCommitments(
+    chain,
+    startingBlock,
+  );
+
+  return shieldCommitments.map(commitment => {
+    const shieldData: ShieldData = {
+      txid: commitment.txid,
+      hash: commitment.hash,
+      timestamp: commitment.timestamp,
+    };
+    return shieldData;
+  });
+};
