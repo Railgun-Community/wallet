99a324aafc63564b5391aa22ff5202e06d4c591d
jrmeurer <jrmeurer@wisc.edu>
2023-09-11 16:37:00 -0600

    Update engine and type for getLatestValidatedRailgunTxid

--- src/services/poi-node/poi-node-railgun-txid.ts
+++ src/services/poi-node/poi-node-railgun-txid.ts
@@ -3,24 +3,15 @@ import {
   GetLatestValidatedRailgunTxid,
 } from '@railgun-community/engine';
 import { POINodes } from './poi-nodes';
-import { isDefined } from '@railgun-community/shared-models';
 
 export const getLatestValidatedRailgunTxid: GetLatestValidatedRailgunTxid =
-  async (chain: Chain): Promise<{ txidIndex: number; merkleroot: string }> => {
+  async (chain: Chain) => {
     // TODO: Use better types
     const route = `/validated-txid/${chain.type}/${chain.id}`;
     const status: {
       validatedTxidIndex: Optional<number>;
       validatedMerkleroot: Optional<string>;
     } = await POINodes.get(route);
-
-    if (
-      !isDefined(status.validatedTxidIndex) ||
-      !isDefined(status.validatedMerkleroot)
-    ) {
-      // Not found
-      return { txidIndex: -1, merkleroot: '' };
-    }
 
     return {
       txidIndex: status.validatedTxidIndex,
