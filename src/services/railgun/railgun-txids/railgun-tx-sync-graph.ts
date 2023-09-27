721e9cb3f98bcfc2b0e28ffeca1ce96d96761c15
jrmeurer <jrmeurer@wisc.edu>
2023-09-27 11:15:16 -0600

    Fix deploy

--- src/services/railgun/railgun-txids/railgun-tx-sync-graph.ts
+++ src/services/railgun/railgun-txids/railgun-tx-sync-graph.ts
@@ -162,6 +162,7 @@ const getBuiltGraphClient = async (
     return mesh;
   } catch (err) {
     throw new Error(
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       `ERROR getting mesh - if error includes "can't generate schema," make sure to check the filepaths for source schema imports in the built index file: ${err.message}`,
     );
   }
