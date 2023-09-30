338d20d6304a9aae224d4b853b30ccc667fc7d09
jrmeurer <jrmeurer@wisc.edu>
2023-09-30 12:46:05 -0600

    Add default poi list

--- src/services/poi/poi-constants.ts
+++ src/services/poi/poi-constants.ts
@@ -1,5 +1,8 @@
 import { POIList, POIListType } from '@railgun-community/engine';
 
 export const POI_REQUIRED_LISTS: POIList[] = [
-  { key: 'test', type: POIListType.Gather },
+  {
+    key: 'efc6ddb59c098a13fb2b618fdae94c1c3a807abc8fb1837c93620c9143ee9e88',
+    type: POIListType.Gather,
+  },
 ];
