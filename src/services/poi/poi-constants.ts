0914fb41a35c12b17ed8c5f7a39f7cf956c55342
jrmeurer <jrmeurer@wisc.edu>
2023-10-17 10:18:03 -0600

    Activate 0xefc poi list

--- src/services/poi/poi-constants.ts
+++ src/services/poi/poi-constants.ts
@@ -3,6 +3,6 @@ import { POIList, POIListType } from '@railgun-community/engine';
 export const POI_REQUIRED_LISTS: POIList[] = [
   {
     key: 'efc6ddb59c098a13fb2b618fdae94c1c3a807abc8fb1837c93620c9143ee9e88',
-    type: POIListType.Gather,
+    type: POIListType.Active,
   },
 ];
