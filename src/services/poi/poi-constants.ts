cfa3e8b3e00aa94dc1ff5a59a90ca5c48fae1ae4
jrmeurer <jrmeurer@wisc.edu>
2023-10-19 13:39:05 -0600

    Add name/description for poi list

--- src/services/poi/poi-constants.ts
+++ src/services/poi/poi-constants.ts
@@ -4,5 +4,8 @@ export const POI_REQUIRED_LISTS: POIList[] = [
   {
     key: 'efc6ddb59c098a13fb2b618fdae94c1c3a807abc8fb1837c93620c9143ee9e88',
     type: POIListType.Active,
+    name: 'Chainalysis OFAC Sanctions API',
+    description:
+      'API which is used to restrict bad actors designated by the US Department of the Treasury. See: https://www.chainalysis.com/free-cryptocurrency-sanctions-screening-tools.',
   },
 ];
