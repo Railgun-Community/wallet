c02b8ad05ad59d4c7ec1f4f574f5d75900a57af6
Pete S <ptaylorsimpson@gmail.com>
2023-11-14 14:40:01 -0800

    Pass through POIValidation

--- src/services/poi/index.ts
+++ src/services/poi/index.ts
@@ -1,3 +1,6 @@
+import { POIValidation } from '@railgun-community/engine';
+
 export * from './poi-required';
 export * from './poi-status-info';
 export * from './poi-node-request';
+export { POIValidation };
