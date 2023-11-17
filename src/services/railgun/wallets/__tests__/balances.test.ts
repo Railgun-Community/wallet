ad8518b712716b797234a8331d84faef092d5660
Pete S <ptaylorsimpson@gmail.com>
2023-11-16 17:03:39 -0800

    Removes refreshRailgunBalances function. scanUpdatesForMerkletreeAndWallets does the same.

--- src/services/railgun/wallets/__tests__/balances.test.ts
+++ src/services/railgun/wallets/__tests__/balances.test.ts
@@ -9,9 +9,8 @@ import {
 import { closeTestEngine, initTestEngine } from '../../../../tests/setup.test';
 import { createRailgunWallet } from '../wallets';
 import {
-  refreshRailgunBalances,
-  rescanFullUTXOMerkletreesAndWallets,
   scanUpdatesForMerkletreeAndWallets,
+  rescanFullUTXOMerkletreesAndWallets,
 } from '../balances';
 import { Chain, ChainType, isDefined } from '@railgun-community/shared-models';
 import { getTestTXIDVersion } from '../../../../tests/helper.test';
@@ -74,25 +73,18 @@ describe('balances', () => {
   });
 
   it('Should run wallet scan if active wallet for ID', async () => {
-    const fullRescan = false;
     const chain: Chain = { type: ChainType.EVM, id: 1 };
-    const response = await refreshRailgunBalances(
-      txidVersion,
-      chain,
-      knownWalletID,
-      fullRescan,
-    );
+    const response = await scanUpdatesForMerkletreeAndWallets(chain);
     expect(response).to.be.undefined;
     expect(walletScanStub.calledOnce).to.be.true;
     expect(engineScanStub.calledOnce).to.be.true;
   });
 
   it('Should throw if no active wallet for ID', async () => {
-    const fullRescan = false;
     const chain: Chain = { type: ChainType.EVM, id: 1 };
-    await expect(
-      refreshRailgunBalances(txidVersion, chain, 'unknown', fullRescan),
-    ).rejectedWith('No RAILGUN wallet for ID');
+    await expect(scanUpdatesForMerkletreeAndWallets(chain)).rejectedWith(
+      'No RAILGUN wallet for ID',
+    );
     expect(walletScanStub.notCalled).to.be.true;
     expect(engineScanStub.notCalled).to.be.true;
   });
