406f1c08fef25b1ec4727de98298f17958dfdf49
jacobmakarsky <jacobmakarsky@gmail.com>
2024-05-24 17:15:44 -0400

    sepolia added in for railgun-txids and polygon replaced with sepolia in balances-live.test.ts

--- src/services/railgun/railgun-txids/__tests__/railgun-txid-sync-graph-v2.test.ts
+++ src/services/railgun/railgun-txids/__tests__/railgun-txid-sync-graph-v2.test.ts
@@ -18,6 +18,11 @@ describe('railgun-txid-sync-graph', () => {
       launchBlock: 1000,
       launchTimestamp: 1000,
     };
+
+    NETWORK_CONFIG[NetworkName.EthereumSepolia].poi = {
+      launchBlock: 1000,
+      launchTimestamp: 1000,
+    };
   });
 
   it('Should pull railgun txs subgraph query - Ethereum', async () => {
@@ -65,4 +70,50 @@ describe('railgun-txid-sync-graph', () => {
       '065bcb1a9d4cfa110f05b480f79f27fe2ad672868d3d1bdec05df2ddaec8333d',
     ]);
   }).timeout(20000);
-  });
+
+  it('Should pull railgun txs subgraph query - Sepolia', async () => {
+    const railgunTxs: RailgunTransactionV2[] =
+      await quickSyncRailgunTransactionsV2(ETH_CHAIN, undefined);
+
+    expect(railgunTxs).to.be.an('array');
+    expect(railgunTxs.length).to.equal(5000);
+
+    expect(railgunTxs[0].commitments).to.deep.equal([
+      '0x1afd01a29faf22dcc5678694092a08d38de99fc97d07b9281fa66f956ce43579',
+      '0x2ffc716d8ae767995961bbde4a208dbae438783065bbd200f51a8d4e97cc2289',
+      '0x078f9824c86b2488714eb76dc15199c3fa21903517d5f3e19ab2035d264400b6',
+    ]);
+    expect(railgunTxs[0].nullifiers).to.deep.equal([
+      '0x1e52cee52f67c37a468458671cddde6b56390dcbdc4cf3b770badc0e78d66401',
+      '0x0ac9f5ab5bcb5a115a3efdd0475f6c22dc6a6841caf35a52ecf86a802bfce8ee',
+    ]);
+    expect(railgunTxs[0].graphID).to.equal(
+      '0x0000000000000000000000000000000000000000000000000000000000e1285000000000000000000000000000000000000000000000000000000000000001500000000000000000000000000000000000000000000000000000000000000000',
+    );
+    expect(railgunTxs[0].boundParamsHash).to.equal(
+      '0x2c72a0bcce4f1169dd988204775483938ded5f5899cec84829b1cc667a683784',
+    );
+    expect(railgunTxs[0].txid).to.equal(
+      'f07a9a458f57f1cc9cc2e5a627c3ef611a18b77e10c2bfc133fceca7743f8d0c',
+    );
+    expect(railgunTxs[0].utxoTreeIn).to.equal(0);
+    expect(railgunTxs[0].utxoTreeOut).to.equal(0);
+    expect(railgunTxs[0].utxoBatchStartPositionOut).to.equal(2);
+    expect(railgunTxs[0].unshield).to.equal(undefined);
+    expect(railgunTxs[0].blockNumber).to.equal(14755920);
+    expect(railgunTxs[0].verificationHash).to.equal(
+      '0x099cd3ebcadaf6ff470d16bc0186fb5f26cd4103e9970effc9b6679478e11c72',
+    );
+  }).timeout(20_000);
+
+  it('Should pull unshield railgun txids - Sepolia', async () => {
+    const unshieldRailgunTxids: string[] = await getRailgunTxidsForUnshields(
+      ETH_CHAIN,
+      '0x0b3b7179df1377c0a13058508e7dff2dbe8f73c39d68f569bc90b1c8b277082e',
+    );
+
+    expect(unshieldRailgunTxids).to.deep.equal([
+      '065bcb1a9d4cfa110f05b480f79f27fe2ad672868d3d1bdec05df2ddaec8333d',
+    ]);
+  }).timeout(20000);
+});
--- src/services/railgun/wallets/__tests__/balances-live.test.ts
+++ src/services/railgun/wallets/__tests__/balances-live.test.ts
@@ -2,7 +2,7 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import {
   MOCK_DB_ENCRYPTION_KEY,
-  MOCK_FALLBACK_PROVIDER_JSON_CONFIG_POLYGON,
+  MOCK_FALLBACK_PROVIDER_JSON_CONFIG_SEPOLIA,
   MOCK_MNEMONIC,
 } from '../../../../tests/mocks.test';
 import {
@@ -31,7 +31,7 @@ let railgunWalletID: string;
 
 const txidVersion = getTestTXIDVersion();
 
-const networkName = NetworkName.Polygon;
+const networkName = NetworkName.EthereumSepolia;
 const chain: Chain = NETWORK_CONFIG[networkName].chain;
 
 describe('balances-live', () => {
@@ -49,7 +49,7 @@ describe('balances-live', () => {
     railgunWalletID = railgunWalletInfo.id;
 
     await loadProvider(
-      MOCK_FALLBACK_PROVIDER_JSON_CONFIG_POLYGON,
+      MOCK_FALLBACK_PROVIDER_JSON_CONFIG_SEPOLIA,
       networkName,
       10_000, // pollingInterval
     );
@@ -61,8 +61,7 @@ describe('balances-live', () => {
     );
     await Promise.all([
       pollUntilUTXOMerkletreeScanned(),
-      // Enable this when running on a network supporting PPOI:
-      // pollUntilTXIDMerkletreeScanned(),
+      pollUntilTXIDMerkletreeScanned(),
     ]);
   });
 
@@ -70,54 +69,58 @@ describe('balances-live', () => {
     await closeTestEngine();
   });
 
-  it('[V2] Should run live balance fetch, transaction history scan, and POI status info scan', async function run() {
-    if (!isV2Test()) {
-      this.skip();
-      return;
-    }
+  it.only(
+    '[V2] Should run live balance fetch, transaction history scan, and POI status info scan',
+    async function run() {
+      if (!isV2Test()) {
+        this.skip();
+        return;
+      }
 
-    await refreshBalances(
-      chain,
-      undefined, // walletIdFilter
-    );
+      await refreshBalances(
+        chain,
+        undefined, // walletIdFilter
+      );
 
-    const wallet = fullWalletForID(railgunWalletID);
-    const balances = await wallet.getTokenBalances(
-      txidVersion,
-      chain,
-      false, // onlySpendable
-    );
-    expect(Object.keys(balances).length).to.be.greaterThanOrEqual(1);
+      const wallet = fullWalletForID(railgunWalletID);
+      const balances = await wallet.getTokenBalances(
+        txidVersion,
+        chain,
+        false, // onlySpendable
+      );
 
-    const transactionHistory = await wallet.getTransactionHistory(
-      chain,
-      undefined,
-    );
-    expect(transactionHistory.length).to.be.greaterThanOrEqual(2);
+      // Note: railgunWallet above needs to perform transactions on above network to have balances
+      expect(Object.keys(balances).length).to.be.greaterThanOrEqual(1);
 
-    const poiStatusReceived = await wallet.getTXOsReceivedPOIStatusInfo(
-      txidVersion,
-      chain,
-    );
-    expect(poiStatusReceived.length).to.be.greaterThanOrEqual(2);
-    expect(poiStatusReceived[0].strings.blindedCommitment).to.not.equal(
-      'Missing',
-    );
+      const transactionHistory = await wallet.getTransactionHistory(
+        chain,
+        undefined,
+      );
+      expect(transactionHistory.length).to.be.greaterThanOrEqual(2);
 
-    // TODO: Uncomment when we have PPOI enabled on the testnet of choice
-    // const txidMerkletree = getTXIDMerkletreeForNetwork(
-    //   txidVersion,
-    //   networkName,
-    // );
-    // expect(txidMerkletree.savedPOILaunchSnapshot).to.equal(true);
+      const poiStatusReceived = await wallet.getTXOsReceivedPOIStatusInfo(
+        txidVersion,
+        chain,
+      );
+      expect(poiStatusReceived.length).to.be.greaterThanOrEqual(2);
+      expect(poiStatusReceived[0].strings.blindedCommitment).to.not.equal(
+        'Missing',
+      );
 
-    // const poiStatusSpent = await wallet.getTXOsSpentPOIStatusInfo(
-    //   txidVersion,
-    //   chain,
-    // );
-    // expect(poiStatusSpent.length).to.be.greaterThanOrEqual(1);
-    // expect(poiStatusSpent[0].strings.railgunTransactionInfo).to.not.equal(
-    //   'Not found',
-    // );
-  }).timeout(90_000);
+      const txidMerkletree = getTXIDMerkletreeForNetwork(
+        txidVersion,
+        networkName,
+      );
+      expect(txidMerkletree.savedPOILaunchSnapshot).to.equal(true);
+
+      const poiStatusSpent = await wallet.getTXOsSpentPOIStatusInfo(
+        txidVersion,
+        chain,
+      );
+      expect(poiStatusSpent.length).to.be.greaterThanOrEqual(1);
+      expect(poiStatusSpent[0].strings.railgunTransactionInfo).to.not.equal(
+        'Not found',
+      );
+    },
+  ).timeout(90_000);
 });
